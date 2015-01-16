1ae319769db45340e3e11044b90489fcd95367f7
danomatika <danomatika@gmail.com>
2015-01-16 12:50:56 -0500

    renamed examples and removed example project files

--- addon_config.mk
+++ addon_config.mk
@@ -22,57 +22,10 @@ meta:
 	ADDON_URL = http://github.com/danomatika/ofxPd
 
 common:
-	# dependencies with other addons, a list of them separated by spaces 
-	# or use += in several lines
-	# ADDON_DEPENDENCIES =
-	
-	# include search paths, this will be usually parsed from the file system
-	# but if the addon or addon libraries need special search paths they can be
-	# specified here separated by spaces or one per line using +=
-	# ADDON_INCLUDES =
-	
-	# any special flag that should be passed to the compiler when using this
-	# addon
-	ADDON_CFLAGS = -DHAVE_UNISTD_H -DUSEAPI_DUMMY -DPD
-	
-	# any special flag that should be passed to the linker when using this
-	# addon, also used for system libraries with -lname
-	# ADDON_LDFLAGS =
-	
-	# linux only, any library that should be included in the project using
-	# pkg-config
-	# ADDON_PKG_CONFIG_LIBRARIES =
-	
-	# osx/iOS only, any framework that should be included in the project
-	# ADDON_FRAMEWORKS =
-	
-	# source files, these will be usually parsed from the file system looking
-	# in the src folders in libs and the root of the addon. if your addon needs
-	# to include files in different places or a different set of files per platform
-	# they can be specified here
-	# ADDON_SOURCES =
-	
-	# some addons need resources to be copied to the bin/data folder of the project
-	# specify here any files that need to be copied, you can use wildcards like * and ?
-	# ADDON_DATA = 
-	
-	# when parsing the file system looking for libraries exclude this for all or
-	# a specific platform
-	# ADDON_LIBS_EXCLUDE =
-	
-	# when parsing the file system looking for sources exclude this for all or
-	# a specific platform
-	# ADDON_SOURCES_EXCLUDE = 
-	
-	# when parsing the file system looking for include paths exclude this for all or
-	# a specific platform
-    # ADDON_INCLUDES_EXCLUDE =
+	# required for libpd
+	ADDON_CFLAGS = -DHAVE_UNISTD_H -DHAVE_ALLOCA_H -DUSEAPI_DUMMY -DPD
 
 win_cb:
-	# when parsing the file system looking for sources exclude this for all or
-	# a specific platform
+	# not sure if pd~ is working on Windows, so leave it out
 	ADDON_SOURCES_EXCLUDE = libs/libpd/pure-data/extra/pd~
-	
-	# when parsing the file system looking for include paths exclude this for all or
-	# a specific platform
-    ADDON_INCLUDES_EXCLUDE = libs/libpd/pure-data/extra/pd~
+	ADDON_INCLUDES_EXCLUDE = libs/libpd/pure-data/extra/pd~
